b2fe0a646038c1e8e88af1cc0fc9a08685ca3036
neerajbachani <neerajbachani2003@gmail.com>
2024-06-16 16:12:18 +0530

    final changes frontend

--- src/Config/ApiConfig.js
+++ src/Config/ApiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:3000"
+export const API_BASE_URL = "https://artishishaapi-production.up.railway.app"
 
 const jwt = localStorage.getItem("jwt")
 
